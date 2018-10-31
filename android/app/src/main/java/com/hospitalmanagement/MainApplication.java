c02e0baf9fcbe9dbae0beed74ebf6a8e4b66f62d
Syed zain Ali <zain.ali@Syeds-Mac-mini-5792.local>
2018-10-31 21:31:53 +0500

    lib commit

--- android/app/src/main/java/com/hospitalmanagement/MainApplication.java
+++ android/app/src/main/java/com/hospitalmanagement/MainApplication.java
@@ -3,6 +3,9 @@ package com.hospitalmanagement;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.opensettings.OpenSettingsPackage;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.github.droibit.android.reactnative.customtabs.CustomTabsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new OpenSettingsPackage(),
+            new RNGoogleSigninPackage(),
+            new CustomTabsPackage()
       );
     }
 
